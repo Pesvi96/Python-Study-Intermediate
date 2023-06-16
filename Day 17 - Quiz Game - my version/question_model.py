e89ff4eb756a73c519eaa7e4e59d759cef029ab0
Pesvi <e.pesvianidze@gmail.com>
2023-06-16 14:47:22 +0400

    Final touch, simplifying my code

--- Day 17 - Quiz Game - my version/question_model.py
+++ Day 17 - Quiz Game - my version/question_model.py
@@ -4,6 +4,7 @@ from data import question_data
 class Questions:
     QUESTION_DATA = question_data
     QUESTION_DATA_LENGTH = len(question_data)
+
     def __init__(self, number):
         self.text = question_data[number]["text"]
         self.answer = question_data[number]["answer"]
--- Day 17 - Quiz Game - my version/quiz_brain.py
+++ Day 17 - Quiz Game - my version/quiz_brain.py
@@ -2,6 +2,12 @@ from question_model import Questions
 from random import choice
 
 
+def check_input(player_input, correct_inputs):
+    while player_input not in correct_inputs:
+        player_input = input(f"Wrong input. Type {', '.join(correct_inputs)}")
+    return player_input
+
+
 class QuizBrain:
     def __init__(self):
         self.questions_list = []
@@ -10,9 +16,7 @@ class QuizBrain:
 
     def play(self):
         # Creates a list of question objects
-        for x in range(Questions.QUESTION_DATA_LENGTH):
-            question = Questions(x)
-            self.questions_list.append(question)
+        self.create_questions_object_list()
 
         # Actual play
         for x in range(len(self.questions_list)):
@@ -21,14 +25,24 @@ class QuizBrain:
             text = current_question.text
             answer = current_question.answer
 
-            player_answer = input(f"Question {x}: {text} ").capitalize()
-            while player_answer not in ["True", "False"]:
-                player_answer = input(f"Wrong input, please try again ").capitalize()
+            player_answer = input(f"Question {x}: {text} ").lower()
+            check_input(player_answer, ["true", "false"])
             print(f"Correct answer is {answer}")
             self.questions_list.remove(current_question)
-            if player_answer == answer:
+            if player_answer == answer.lower():
                 self.player_score += 1
             self.round += 1
             print(f"Ongoing score: {self.player_score}/{self.round}\n")
 
-        print(f"\n\nEnd of game! Your last score was {self.player_score}/{self.round}")
+        print(f"\n\nEnd of game! Your last score was {self.player_score}/{self.round}\n")
+
+    def create_questions_object_list(self):
+        for x in range(Questions.QUESTION_DATA_LENGTH):
+            question = Questions(x)
+            self.questions_list.append(question)
+
+    def play_again(self):
+        play_again = input(f"Wanna play again? Type 'y' or 'n' ").lower()
+        check_input(play_again, ["y", "n"])
+        if play_again == "n":
+            return False

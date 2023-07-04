d64e85390c012cfd70ea7ff9e029a176c8150bc6
Pesvi <e.pesvianidze@gmail.com>
2023-07-04 20:04:13 +0400

    Day 18

--- Day 19 - Etch-A-Sketch and Races/Turtle Race.py
+++ Day 19 - Etch-A-Sketch and Races/Turtle Race.py
@@ -1,29 +1,36 @@
 from turtle import Turtle, Screen
-from random import choice
+from random import choice, randint
 
 screen = Screen()
 racers = []
-colors = ["Blue", "Red", "Black", "Purple", "Green"]
+colors = ["Blue", "Red", "Black", "Purple", "Green", "Violet", "Orange", "Magenta"]
+
+
+def create_racers(amount):
+    for x in range(amount):
+        racers.append(Turtle())
+        color = choice(colors)
+        racers[x].color(color)
+        racers[x].shape("turtle")
+        racers[x].penup()
+        racers[x].goto(-200, -150+(x*50))
+        colors.remove(color)
+
+def race(amount):
+    for x in range(amount):
+        racers[x].forward(randint(10,50))
 
 
 
-for x in range(5):
-    racers.append(Turtle())
-    color = choice(colors)
-    racers[x].color(color)
-    racers[x].shape("turtle")
-    racers[x].penup()
-    racers[x].goto(-200, -100+(x*50))
-    colors.remove(color)
 
+racers_amount = 8
+create_racers(racers_amount)
+while True:
+    race(racers_amount)
 
 
 
 print(racers)
 
-# bobby = Turtle()
-# bobby.fillcolor("DarkRed")
-# bobby.shape("turtle")
-# bobby.pensize(10)
 
 screen.exitonclick()

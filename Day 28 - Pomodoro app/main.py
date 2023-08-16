3d5c6cf63529153133642de13526aa5ba9156a08
Pesvi <e.pesvianidze@gmail.com>
2023-08-16 15:09:44 +0400

    End of day day 28

--- Day 28 - Pomodoro app/main.py
+++ Day 28 - Pomodoro app/main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import time
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -7,28 +6,68 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 0.1
+SHORT_BREAK_MIN = 0.05
+LONG_BREAK_MIN = 0.2
+reps = 1
+check_symbol = "✓"
+check_string = ""
+timer_com = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    global check_string
+    global timer_com
+    window.after_cancel(timer_com)
+    check_string = ""
+    title.config(text="Timer", fg=GREEN)
+    canvas.itemconfigure(timer, text="00:00")
+    reps = 1
+    # If you want loop start_countdown
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_countdown():
+    global reps
+    # while reps < 8:
+    print(f"now on rep: {reps}")
+    work_sec = int(WORK_MIN * 60)
+    short_break_sec = int(SHORT_BREAK_MIN * 60)
+    long_break_sec = int(LONG_BREAK_MIN * 60)
+    if reps in [1, 3, 5, 7]:
+        title.config(text="Work")
+        countdown(work_sec)
+    elif reps in [2, 4, 6]:
+        title.config(text="Break", fg=PINK)
+        countdown(short_break_sec)
+    elif reps == 8:
+        title.config(text="Break", fg=RED)
+        countdown(long_break_sec)
+    elif reps > 8:
+        reset_timer()
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def countdown():
-    countdown_time = int(canvas.itemcget(timer, "text"))
-    countdown_time -= 1
-    canvas.itemconfigure(timer, text=countdown_time)
 
+def countdown(count):
+    global reps
+    global check_string
+    global timer_com
+    count_min = "{:02}".format(count // 60)
+    count_sec = "{:02}".format(count % 60)
+    if count >= 0:
+        canvas.itemconfigure(timer, text=f"{str(count_min)}:{str(count_sec)}")
+        timer_com = window.after(1000, countdown, count - 1)
+    else:
+        # Timer is done, you can perform any necessary actions here
+        reps += 1
+        start_countdown()
+        if reps % 2 == 0:
+            check_string += check_symbol
+            checkmark.config(text=check_string)
 
-def start_countdown():
-    for _ in range(4):
-        window.after(1000, countdown)
-
-
-# TODO: why doesn't start_countdown don't work? The screen resets when the function is done. Video 257
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -47,22 +86,20 @@ tomato_png = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_png)
 timer = canvas.create_text(100, 130, text="5", fill="white", font=(FONT_NAME, 35, "bold"))
 
-
-
-
-
 canvas.grid(row=1, column=1)
 
 start_btn = Button(text="Start", bg=YELLOW, highlightbackground=YELLOW, command=start_countdown)
 start_btn.grid(row=2, column=0)
 
-reset_btn = Button(text="Reset", bg=YELLOW, highlightbackground=YELLOW)
+reset_btn = Button(text="Reset", bg=YELLOW, highlightbackground=YELLOW, command=reset_timer)
 reset_btn.grid(row=2, column=2)
 
 title = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 45), bg=YELLOW)
 title.grid(row=0, column=1)
 
-checkmark = Label(text="✓", fg=GREEN, font=(FONT_NAME, 30, "bold"), bg=YELLOW)
+checkmark = Label(fg=GREEN, font=(FONT_NAME, 30, "bold"), bg=YELLOW)
 checkmark.grid(row=3, column=1)
 
+print("before")
 window.mainloop()
+print("after")
---
+++ test.py
@@ -0,0 +1,4 @@
+numb = 9
+if 0 <= numb <= 9:
+    numb = f"0{numb}"
+    print(numb)

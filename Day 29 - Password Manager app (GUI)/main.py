081bf6aefcdf7d293ad41ac12eb10e918a9bf531
Pesvi <e.pesvianidze@gmail.com>
2023-08-18 16:10:04 +0400

    file write doesn't work

--- Day 29 - Password Manager app (GUI)/main.py
+++ Day 29 - Password Manager app (GUI)/main.py
@@ -2,6 +2,7 @@ import tkinter.messagebox
 from tkinter import *
 from symbols import source
 from random import choice, randint, shuffle
+import panda as pd
 
 """
     input fields:
@@ -17,61 +18,11 @@ Generate random pass - generate and ✅
     Additional functions:
 popups - confirmation popup ✅
         empty field error popup ✅
+        
 
 """
 
-
-def create_ui():
-    """Creates the window and all the elements for this prog
-    :return: window, web_label, email_label, pass_label, web_input, email_input, pass_input, generate_btn, add_btn
-    """
-    BACKGROUND = "#EEE0C9"
-    BLACK = "#000000"
-    WHITE = "#FFFFFF"
-
-    window = Tk()
-    window.title("Password Manager")
-    window.minsize(width=600, height=400)
-    window.config(bg=BACKGROUND, padx=30, pady=30)
-
-    # ----------------- Widgets --------------------
-
-    web_label = Label(text="Website:", fg=BLACK, bg=BACKGROUND)
-    web_label.place(x=64, y=200)
-
-    email_label = Label(text="Email/Username:", fg=BLACK, bg=BACKGROUND)
-    email_label.place(x=38, y=230)
-
-    # You could do it with .grid() and use columnspan(რამდენ გრიდს ეხება) if you are using a long widget
-
-    pass_label = Label(text="Password:", fg=BLACK, bg=BACKGROUND)
-    pass_label.place(x=59, y=260)
-
-    web_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=33, insertbackground=BLACK,
-                      exportselection=True)  # TODO: რაცხა არ მუშაობს
-    web_input.place(x=160, y=200)
-
-    email_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=33, insertbackground=BLACK)
-    email_input.place(x=160, y=230)
-
-    pass_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=15, insertbackground=BLACK, show='*')
-    pass_input.place(x=160, y=260)
-
-    generate_btn = Button(fg=BLACK, highlightbackground=BACKGROUND, highlightthickness=0,
-                          text="Generate Password", command=generate_pass)
-    generate_btn.place(x=312, y=255)
-
-    add_btn = Button(fg=BLACK, highlightbackground=BACKGROUND, highlightthickness=0, text="Add", width=31, command=add)
-    add_btn.place(x=155, y=285)
-
-    canvas = Canvas(width=200, height=200, bg=BACKGROUND, highlightthickness=0)
-    logo = PhotoImage(file="logo.png")
-    canvas.create_image(100, 100, image=logo)
-    canvas.place(x=180, y=0)
-
-    return window, web_label, email_label, pass_label, web_input, email_input, \
-        pass_input, generate_btn, add_btn, canvas, logo
-
+# TODO: Write into file left, it's not working for now
 
 def generate_pass():
     password = []
@@ -130,7 +81,60 @@ def add():
         email_input.delete(0, 20)
         pass_input.delete(0, 20)
         with open(f"./info", mode="r+") as file:
+            print("writing into file")
             file.write(f"{web}: {mail}, {password}\n")
+
+
+def create_ui():
+    """Creates the window and all the elements for this prog
+    :return: window, web_label, email_label, pass_label, web_input, email_input, pass_input, generate_btn, add_btn
+    """
+    BACKGROUND = "#EEE0C9"
+    BLACK = "#000000"
+    WHITE = "#FFFFFF"
+
+    window = Tk()
+    window.title("Password Manager")
+    window.minsize(width=600, height=400)
+    window.config(bg=BACKGROUND, padx=30, pady=30)
+
+    # ----------------- Widgets --------------------
+
+    web_label = Label(text="Website:", fg=BLACK, bg=BACKGROUND)
+    web_label.place(x=64, y=200)
+
+    email_label = Label(text="Email/Username:", fg=BLACK, bg=BACKGROUND)
+    email_label.place(x=38, y=230)
+
+    # You could do it with .grid() and use columnspan(რამდენ გრიდს ეხება) if you are using a long widget
+
+    pass_label = Label(text="Password:", fg=BLACK, bg=BACKGROUND)
+    pass_label.place(x=59, y=260)
+
+    web_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=33, insertbackground=BLACK,
+                      exportselection=True)  # TODO: რაცხა არ მუშაობს
+    web_input.place(x=160, y=200)
+
+    email_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=33, insertbackground=BLACK)
+    email_input.place(x=160, y=230)
+
+    pass_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=15, insertbackground=BLACK, show='*')
+    pass_input.place(x=160, y=260)
+
+    generate_btn = Button(fg=BLACK, highlightbackground=BACKGROUND, highlightthickness=0,
+                          text="Generate Password", command=generate_pass)
+    generate_btn.place(x=312, y=255)
+
+    add_btn = Button(fg=BLACK, highlightbackground=BACKGROUND, highlightthickness=0, text="Add", width=31, command=add)
+    add_btn.place(x=155, y=285)
+
+    canvas = Canvas(width=200, height=200, bg=BACKGROUND, highlightthickness=0)
+    logo = PhotoImage(file="logo.png")
+    canvas.create_image(100, 100, image=logo)
+    canvas.place(x=180, y=0)
+
+    return window, web_label, email_label, pass_label, web_input, email_input, \
+        pass_input, generate_btn, add_btn, canvas, logo
 
 
 window, web_label, email_label, pass_label, web_input, \

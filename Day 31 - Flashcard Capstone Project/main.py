37a20913c1a35b35b2859a78f985990d04587ff6
Pesvi <e.pesvianidze@gmail.com>
2023-09-08 18:22:00 +0400

    Completed Capstone project

--- Day 31 - Flashcard Capstone Project/main.py
+++ Day 31 - Flashcard Capstone Project/main.py
@@ -1,75 +1,90 @@
 from tkinter import *
+import pandas as pd
 import tkinter.messagebox
+from random import choice
+
+# def create_ui():
+#     """Creates the window and all the elements for this prog
+#     :return: window, web_label, email_label, pass_label, web_input, email_input, pass_input, generate_btn, add_btn
+#     """
+
+try:
+    df = pd.read_csv("./data/words.csv")
+    words_list = df.to_dict(orient="records")
+    print(words_list)
+except FileNotFoundError:
+    df = pd.read_csv("./data/french_words.csv")
+    words_list = df.to_dict(orient="records")
+    print(words_list)
+
+BACKGROUND = "#B1DDC6"
+BLACK = "#000000"
+WHITE = "#FFFFFF"
+timer = "after#0"
 
 
+def i_know():
+    global current_card
+    if current_card in words_list:
+        words_list.remove(current_card)
+        data = pd.DataFrame(words_list)
+        data.to_csv("data/words.csv")
 
-BACKGROUND_COLOR = "#B1DDC6"
+    next_card()
 
-def create_ui():
-    """Creates the window and all the elements for this prog
-    :return: window, web_label, email_label, pass_label, web_input, email_input, pass_input, generate_btn, add_btn
-    """
-    BACKGROUND = "#B1DDC6"
-    BLACK = "#000000"
-    WHITE = "#FFFFFF"
 
+def next_card():
+    global timer, current_card
+    window.after_cancel(timer)
+    current_card = choice(words_list)
+    canvas.itemconfig(card_title, text="French", fill=BLACK)
+    canvas.itemconfig(card_word, text=current_card["French"], fill=BLACK)
+    canvas.itemconfig(card_side, image=card_front)
+    timer = window.after(3000, flip_card, current_card)
+
+
+def flip_card(current_card):
+    canvas.itemconfig(card_title, text="English", fill=WHITE)
+    canvas.itemconfig(card_word, text=current_card["English"], fill=WHITE)
+    canvas.itemconfig(card_side, image=card_back)
+
+
+def create():
     window = Tk()
     window.title("Password Manager")
-    window.minsize(width=700, height=500)
-    window.config(bg=BACKGROUND, padx=30, pady=30)
-    #
-    # # ----------------- Widgets --------------------
+    window.minsize(width=900, height=700)
+    window.config(bg=BACKGROUND, padx=50, pady=50)
 
-    canvas = Canvas(width=300, height=300, bg=BACKGROUND, highlightthickness=0)
-    card_front = PhotoImage(file="./images/right.png")
-    canvas.create_image(100, 100, image=card_front)
-    canvas.place(x=200, y=100)
+    # ----------------- Widgets --------------------
 
+    canvas = Canvas(width=800, height=526, bg=BACKGROUND, highlightthickness=0)
+    card_front = PhotoImage(file="./images/card_front.png")
+    card_back = PhotoImage(file="./images/card_back.png")
+    accept_img = PhotoImage(file="./images/right.png")
+    reject_img = PhotoImage(file="./images/wrong.png")
 
+    global current_card
+    current_card = choice(words_list)
 
-    web_label = Label(text="Website:", fg=BLACK, bg=BACKGROUND)
-    web_label.place(x=64, y=200)
+    card_side = canvas.create_image(400, 275, image=card_back)
+    card_title = canvas.create_text(400, 150, text="English",
+                                    fill=WHITE, font=("Ariel", 40, "italic"), justify=CENTER)
+    card_word = canvas.create_text(400, 263, text=current_card["English"],
+                                   fill=WHITE, font=("Ariel", 60, "bold"), justify=CENTER)
+    canvas.grid(row=0, column=0, columnspan=2)
 
-    # email_label = Label(text="Email/Username:", fg=BLACK, bg=BACKGROUND)
-    # email_label.place(x=38, y=230)
-    #
-    # # You could do it with .grid() and use columnspan(რამდენ გრიდს ეხება) if you are using a long widget
-    #
-    # pass_label = Label(text="Password:", fg=BLACK, bg=BACKGROUND)
-    # pass_label.place(x=59, y=260)
-    #
-    # web_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=15, insertbackground=BLACK,
-    #                   exportselection=True)
-    # web_input.place(x=160, y=200)
-    # web_input.focus()
-    #
-    # email_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=33, insertbackground=BLACK)
-    # email_input.place(x=160, y=230)
-    #
-    # pass_input = Entry(bg=WHITE, fg=BLACK, highlightthickness=0, width=15, insertbackground=BLACK, show='*')
-    # pass_input.place(x=160, y=260)
-    #
-    # generate_btn = Button(fg=BLACK, highlightbackground=BACKGROUND, highlightthickness=0,
-    #                       text="Generate Password", command=generate_pass)
-    # generate_btn.place(x=312, y=255)
-    #
-    # search_btn = Button(fg=BLACK, highlightbackground=BACKGROUND, highlightthickness=0,
-    #                     text="Search", width=13, command=search)
-    # search_btn.place(x=312, y=195)
-    #
-    # add_btn = Button(fg=BLACK, highlightbackground=BACKGROUND, highlightthickness=0, text="Add", width=31, command=add)
-    # add_btn.place(x=155, y=285)
-    #
-    # canvas = Canvas(width=200, height=200, bg=BACKGROUND, highlightthickness=0)
-    # logo = PhotoImage(file="logo.png")
-    # canvas.create_image(100, 100, image=logo)
-    # canvas.place(x=180, y=0)
-    #
-    # return window, web_label, email_label, pass_label, web_input, email_input, \
-    #     pass_input, generate_btn, add_btn, canvas, logo  # Could have separated creation of
-    # widgets into categories as well, for example create buttons and create labels
-    return window
+    accept_btn = Button(image=accept_img, highlightbackground=BACKGROUND,
+                        highlightthickness=0, command=i_know)
+    accept_btn.grid(row=1, column=0)
+
+    reject_btn = Button(image=reject_img, highlightbackground=BACKGROUND,
+                        highlightthickness=0, command=next_card)
+    reject_btn.grid(row=1, column=1)
+
+    return window, canvas, card_front, card_back, accept_img, \
+        reject_img, accept_btn, reject_btn, card_word, card_title, card_side
 
 
-window = create_ui()
+window, canvas, card_front, card_back, accept_img, reject_img, \
+    accept_btn, reject_btn, card_word, card_title, card_side = create()
 window.mainloop()
